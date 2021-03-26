a3cc3f0327eaf190e8859511665e307e2717d91d
rango527 <web.mobile497373@gmail.com>
2021-03-26 12:32:59 -0400

    removed unused vars

--- src/containers/SessionContainer.js
+++ src/containers/SessionContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import SessionTable from "../components/SessionTable"
 import MatchesTable from "../components/MatchesTable"
 import { Loader } from "semantic-ui-react"
-import { baseUrl, HEADERS } from "../constants"
+import { baseUrl } from "../constants"
 
 export default class SessionContainer extends Component {
   state = {
@@ -12,17 +12,12 @@ export default class SessionContainer extends Component {
   }
 
   fetchSessions = () => {
-    // let token = localStorage.getItem('token')
-    // if (token) {
-    fetch(baseUrl + "/sessions", {
-      // headers: HEADERS,
-    })
+    fetch(baseUrl + "/sessions", {})
       .then((res) => res.json())
       .then((sessions) => {
         this.setState({ sessions })
       })
       .catch((e) => console.error(e))
-    // }
   }
 
   handleSessionClick = (e, session_id) => {
