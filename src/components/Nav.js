94225ab34004cb8d62c55d71a6020c43b41f98a3
rango527 <web.mobile497373@gmail.com>
2021-03-30 20:01:47 -0400

    fixed nav bug where results tab was active when only record-results should be

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,19 +12,19 @@ export default class Nav extends Component {
         <Link to="/players">
           <Menu.Item
             name="players"
-            active={location.pathname.indexOf("players") > -1}
+            active={location.pathname.indexOf("/players") > -1}
           />
         </Link>
         <Link to="/groups">
           <Menu.Item
             name="groups"
-            active={location.pathname.indexOf("groups") > -1}
+            active={location.pathname.indexOf("/groups") > -1}
           />
         </Link>
         <Link to="/results">
           <Menu.Item
             name="results"
-            active={location.pathname.indexOf("results") > -1}
+            active={location.pathname.indexOf("/results") > -1}
           />
         </Link>
         {isAdmin() ? (
@@ -32,7 +32,7 @@ export default class Nav extends Component {
             <Menu.Item
               name="record results"
               icon="calculator"
-              active={location.pathname.indexOf("record-results") > -1}
+              active={location.pathname.indexOf("/record-results") > -1}
             />
           </Link>
         ) : null}
