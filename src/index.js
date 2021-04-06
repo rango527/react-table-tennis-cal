8aff7e1fd35bf81cb74641d11170f60526092273
rango527 <web.mobile497373@gmail.com>
2021-04-06 15:04:50 -0400

    installed react-query and performed initial setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { QueryClientProvider, QueryClient } from "react-query"
 
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css"
 
+const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
 
 // If you want to start measuring performance in your app, pass a function
