ec2848eba44f49a799e436f9a3aa0d54dcc8e585
rango527 <web.mobile497373@gmail.com>
2021-04-09 12:19:50 -0400

    added nav semantic html element

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,34 +5,36 @@ import { isAdmin } from "../utilities"
 
 export default function Nav({ location }) {
   return (
-    <Menu tabular stackable>
-      <Link to="/players">
-        <Menu.Item
-          name="players"
-          active={location.pathname.indexOf("/players") > -1}
-        />
-      </Link>
-      <Link to="/groups">
-        <Menu.Item
-          name="groups"
-          active={location.pathname.indexOf("/groups") > -1}
-        />
-      </Link>
-      <Link to="/results">
-        <Menu.Item
-          name="results"
-          active={location.pathname.indexOf("/results") > -1}
-        />
-      </Link>
-      {isAdmin() ? (
-        <Link to="/record-results">
+    <nav>
+      <Menu tabular stackable>
+        <Link to="/players">
           <Menu.Item
-            name="record results"
-            icon="calculator"
-            active={location.pathname.indexOf("/record-results") > -1}
+            name="players"
+            active={location.pathname.indexOf("/players") > -1}
           />
         </Link>
-      ) : null}
-    </Menu>
+        <Link to="/groups">
+          <Menu.Item
+            name="groups"
+            active={location.pathname.indexOf("/groups") > -1}
+          />
+        </Link>
+        <Link to="/results">
+          <Menu.Item
+            name="results"
+            active={location.pathname.indexOf("/results") > -1}
+          />
+        </Link>
+        {isAdmin() ? (
+          <Link to="/record-results">
+            <Menu.Item
+              name="record results"
+              icon="calculator"
+              active={location.pathname.indexOf("/record-results") > -1}
+            />
+          </Link>
+        ) : null}
+      </Menu>
+    </nav>
   )
 }
